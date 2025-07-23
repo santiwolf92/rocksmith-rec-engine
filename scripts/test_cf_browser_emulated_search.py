c88e021433ec7b2d772e9c4da1fcb639d8d1d2cb
santiwolf92 <santilobo123@gmail.com>
2025-07-23 20:08:12 -0300

    Update test_cf_browser_emulated_search.py

--- scripts/test_cf_browser_emulated_search.py
+++ scripts/test_cf_browser_emulated_search.py
@@ -2,7 +2,7 @@ import requests
 from urllib.parse import quote_plus
 
 # === CONFIGURATION ===
-SEARCH_TERM = "Paranoid"
+SEARCH_TERM = "Black Sabbath Paranoid"
 ENCODED_TERM = quote_plus(SEARCH_TERM.lower())
 
 # === HEADERS & COOKIES ===
